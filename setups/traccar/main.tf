32dfdf7164fa4d785f46333282116ad393c0a030
Eric de Thouars <eric@thouars.nl>
2022-03-17 11:32:43 +0100

    setups added for traccar and taygetos

--- setups/traccar/main.tf
+++ setups/traccar/main.tf
@@ -5,29 +5,27 @@ variable "netbox_api_token" {}
 variable "vm_private_key_location" {}
 
 
-module "dmzserver1" {
+module "traccar" {
   // Mandatory parameters
   source                  = "../../modules/proxmox_lxc"
   pve_api_url             = var.pve_api_url // defined as TF_VAR_...
   netbox_api_url          = var.netbox_api_url
   netbox_api_token        = var.netbox_api_token
   vm_private_key_location = var.vm_private_key_location
-  vm_vmid                 = 666
-  vm_hostname             = "dmzserver1"
+  vm_hostname             = "traccar"
   vm_role                 = "Server"
   vm_osplatform           = "Ubuntu 21.10 - impish"
   vm_ostemplate           = "ds218:vztmpl/ubuntu-21.10-standard_21.10-1_amd64.tar.zst"
-  vm_tags                 = ["ubuntu_21_10_impish", "docker"]
+  vm_tags                 = ["ubuntu_21_10_impish"]
   vm_ssh_publickey        = <<-EOT
   ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAptygarYRdcrn4MB+ZMR4Ukaqr7+rMYIdYRhpp+bOI4Uhffo0Y5C05QKjHsWzGlfMdzxhaXwCnBQOCugzeBuV0VcBdeqbMiD8/5FQ81LzN+7ce2Rq7cnYWigKogJB5pNwVrrHSGHzJKJLCf6aPHCtaCpJ2TLnmooziYYTL1hN5c6RDC/zjJORyB0qBgiOk5t236Zyjf0s1LdlwCrDdzUPstOXMujVGrmmzCAbTH/OCNMLh1M5IZ9X/HxMpIIpPE7Cqa8NzMUlEX4mwCFHkbkoFG+gzghABL86sp0LPtNJ59RURh2tTJUAuiqA2iLfbdBajEIfVtX30c78T5BNwVFyAQ==
   EOT
-  vm_network_ip           = "192.168.66.20/24"
-  vm_network_gw           = "192.168.66.1"
-  vm_network_bridge       = "vmbr666"
+  vm_network_ip           = "192.168.55.118/24"
+  vm_network_gw           = "192.168.55.1"
 
   // Optional parameters
-  vm_memory           = 1024
-  vm_unprivileged     = false
-  vm_features_nesting = true
+  vm_vmid        = 810
+  vm_memory      = 1024
+  vm_rootfs_size = 8
 }
 
