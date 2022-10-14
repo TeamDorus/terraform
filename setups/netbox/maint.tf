1e5f11d51b3956c2697e3ade610c3e7699ea2ed6
Eric <eric@pop-os3.bartok.thouars.nl>
2022-10-14 12:18:21 +0200

    Changed NAS stroage from ds218 to Vault

--- setups/netbox/maint.tf
+++ setups/netbox/maint.tf
@@ -15,7 +15,7 @@ module "netbox" {
   vm_hostname             = "netbox"
   vm_role                 = "Server"
   vm_osplatform           = "Ubuntu 21.10 - impish"
-  vm_ostemplate           = "ds218:vztmpl/ubuntu-21.10-standard_21.10-1_amd64.tar.zst"
+  vm_ostemplate           = "Vault:vztmpl/ubuntu-21.10-standard_21.10-1_amd64.tar.zst"
   vm_tags                 = ["ubuntu_21_10_impish", "docker"]
   vm_ssh_publickey        = <<-EOT
   ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAptygarYRdcrn4MB+ZMR4Ukaqr7+rMYIdYRhpp+bOI4Uhffo0Y5C05QKjHsWzGlfMdzxhaXwCnBQOCugzeBuV0VcBdeqbMiD8/5FQ81LzN+7ce2Rq7cnYWigKogJB5pNwVrrHSGHzJKJLCf6aPHCtaCpJ2TLnmooziYYTL1hN5c6RDC/zjJORyB0qBgiOk5t236Zyjf0s1LdlwCrDdzUPstOXMujVGrmmzCAbTH/OCNMLh1M5IZ9X/HxMpIIpPE7Cqa8NzMUlEX4mwCFHkbkoFG+gzghABL86sp0LPtNJ59RURh2tTJUAuiqA2iLfbdBajEIfVtX30c78T5BNwVFyAQ==
--- setups/taygetos/main.tf
+++ setups/taygetos/main.tf
@@ -15,7 +15,7 @@ module "taygetos" {
   vm_hostname             = "taygetos"
   vm_role                 = "Server"
   vm_osplatform           = "Ubuntu 22.04 - jammy"
-  vm_ostemplate           = "ds218:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  vm_ostemplate           = "Vault:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
   vm_tags                 = ["ubuntu_22_04_jammy", "docker"]
   vm_ssh_publickey        = <<-EOT
   ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAptygarYRdcrn4MB+ZMR4Ukaqr7+rMYIdYRhpp+bOI4Uhffo0Y5C05QKjHsWzGlfMdzxhaXwCnBQOCugzeBuV0VcBdeqbMiD8/5FQ81LzN+7ce2Rq7cnYWigKogJB5pNwVrrHSGHzJKJLCf6aPHCtaCpJ2TLnmooziYYTL1hN5c6RDC/zjJORyB0qBgiOk5t236Zyjf0s1LdlwCrDdzUPstOXMujVGrmmzCAbTH/OCNMLh1M5IZ9X/HxMpIIpPE7Cqa8NzMUlEX4mwCFHkbkoFG+gzghABL86sp0LPtNJ59RURh2tTJUAuiqA2iLfbdBajEIfVtX30c78T5BNwVFyAQ==
