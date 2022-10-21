06ca6695fe2e161c3809dca2540d280b8016b0ba
Eric <eric@pop-os3.bartok.thouars.nl>
2022-10-21 17:11:53 +0200

    Update to Ubuntu 22.04 Jammy

--- setups/traccar/main.tf
+++ setups/traccar/main.tf
@@ -14,9 +14,9 @@ module "traccar" {
   vm_private_key_location = var.vm_private_key_location
   vm_hostname             = "traccar"
   vm_role                 = "Server"
-  vm_osplatform           = "Ubuntu 21.10 - impish"
-  vm_ostemplate           = "Vault:vztmpl/ubuntu-21.10-standard_21.10-1_amd64.tar.zst"
-  vm_tags                 = ["ubuntu_21_10_impish"]
+  vm_osplatform           = "Ubuntu 22.04 - jammy"
+  vm_ostemplate           = "Vault:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  vm_tags                 = ["ubuntu_22_04_jammy", "docker"]
   vm_ssh_publickey        = <<-EOT
   ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAptygarYRdcrn4MB+ZMR4Ukaqr7+rMYIdYRhpp+bOI4Uhffo0Y5C05QKjHsWzGlfMdzxhaXwCnBQOCugzeBuV0VcBdeqbMiD8/5FQ81LzN+7ce2Rq7cnYWigKogJB5pNwVrrHSGHzJKJLCf6aPHCtaCpJ2TLnmooziYYTL1hN5c6RDC/zjJORyB0qBgiOk5t236Zyjf0s1LdlwCrDdzUPstOXMujVGrmmzCAbTH/OCNMLh1M5IZ9X/HxMpIIpPE7Cqa8NzMUlEX4mwCFHkbkoFG+gzghABL86sp0LPtNJ59RURh2tTJUAuiqA2iLfbdBajEIfVtX30c78T5BNwVFyAQ==
   EOT
